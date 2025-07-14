e57148edf8174a247bbc1e2f698540bfd55323de
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-07-14 19:46:29 +0530

    Dummy Auth middleware for user with different apporach

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const PORT = 3000;
 const app = express();
-const {adminAuth} = require('./middlewares/auth');
+const {adminAuth,userAuth} = require('./middlewares/auth');
 
 app.use("/admin",adminAuth);
+
+app.get("/user/data",userAuth,(req,res)=>{
+    res.send("User Data sent");
+})
 app.get("/admin/getAllData",(req,res)=>{
     res.send("All data sent");
 })
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,6 +9,18 @@ const adminAuth = (req,res,next)=>{
         next();
     }
 }
+const userAuth = (req,res,next)=>{
+    const token = "xyz";
+    const isUser = token === "xyz";
+    console.log("Auth is checking User");
+    if(!isUser){
+        res.status(401).send("Unauthorized Access");
+    }
+    else{
+        next();
+    }
+}
 module.exports = {
-    adminAuth
+    adminAuth,
+    userAuth
 }
