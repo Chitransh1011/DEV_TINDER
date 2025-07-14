db9aa480caac2d439cbcd6a519a83c5cac8a5a51
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-07-14 17:44:54 +0530

    Multiple Route Handler array case

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const PORT = 3000;
 const app = express();
-app.use("/routes",(req,res,next)=>{
+app.use("/routes",[(req,res,next)=>{
     console.log("Route Handler 1");
     next();
 },(req,res,next)=>{
     console.log("Route Handler 2");
-    res.send("Route Handler 2");
+    next();
+}],(req,res,next)=>{
+    console.log("Route Handler 3");
+    res.send("Route Handler 3");
 })
 
 
