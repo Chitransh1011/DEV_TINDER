38961f426af53fa2ea355d361a9dff0f810b555d
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-07-14 16:19:17 +0530

    Routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const PORT = 3000;
 const app = express();
-
-app.use((req,res)=>{
-    res.send("Hi From the server");
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Chitransh",lastName:"Prasanna"});
 })
+app.post("/user",(req,res)=>{
+    res.send("Successfully pushed in Database");
+})
+app.delete("/user",(req,res)=>{
+    res.send("Successfully deleted from Database");
+})
+
 app.listen(PORT,()=>{
     console.log(`Server started on PORT : ${PORT}`);
 })
