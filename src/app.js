72ad87744fecadb4cb6538e4c7044a4d6769221a
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-07-15 08:42:21 +0530

    trycatch in handling error while saving in db

--- src/app.js
+++ src/app.js
@@ -1,31 +1,36 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const {PORT} = require('./config/serverConfig');
-const {adminAuth,userAuth} = require('./middlewares/auth');
+const { PORT } = require("./config/serverConfig");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 const User = require("./models/user");
 const app = express();
 
 // DUMMY DATA
 const data = {
-    firstName:"Virat",
-    lastName:"Kohli",
-    emailId:"virat@gmail.com",
-    password:"virat@123",
-    age:35,
-    gender:"Male"
+  firstName: "Virat",
+  lastName: "Kohli",
+  emailId: "virat@gmail.com",
+  password: "virat@123",
+  age: 35,
+  gender: "Male",
 };
-app.post("/signup",async (req,res)=>{
+app.post("/signup", async (req, res) => {
+  try {
     const user = new User(data);
     await user.save();
     res.send("User signuped successfully");
-})
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-connectDB().then(()=>{
+connectDB()
+  .then(() => {
     console.log("Database connected successfully");
-    app.listen(PORT,()=>{
-    console.log(`Server started on PORT : ${PORT}`);
-})
-})
-.catch(()=>{
+    app.listen(PORT, () => {
+      console.log(`Server started on PORT : ${PORT}`);
+    });
+  })
+  .catch(() => {
     console.error("Database not connected");
-})
+  });
