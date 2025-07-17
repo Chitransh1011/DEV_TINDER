1cfb1766698fe574c03c26a4d8f96a84fc965d9a
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-07-17 16:20:16 +0530

    Added api for forgot password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const User = require("../models/user");
 const { validationEditProfile } = require("../utils/validation");
+const bcrypt = require("bcrypt");
+const validator = require('validator')
 const profileRouter = express.Router();
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
@@ -25,4 +27,22 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     res.status(400).send(error.message);
   }
 });
+profileRouter.patch("/profile/password", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId });
+    if(!user){
+        throw new Error("Invalid User");
+    }
+    if(!validator.isStrongPassword(req.body.updatedPassword)){
+        throw new Error("Please Enter Strong password");
+    }
+    const newpassword = await bcrypt.hash(req.body.updatedPassword, 10);
+    user.password = newpassword;
+    await user.save();
+    res.send("Password Updated");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
 module.exports = profileRouter;
