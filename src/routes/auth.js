b2153f949a1c070024ce132e8fd53d6916d1dd71
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-07-17 15:57:33 +0530

    Added logout logic

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -53,5 +53,11 @@ authRouter.post("/login", async (req, res) => {
     res.status(404).send(error.message);
   }
 });
+authRouter.post("/logout",async(req,res)=>{
+    res.cookie('token',null,{
+      expires: new Date(Date.now())
+    });
+    res.send("LogOut Successfully");
+})
 
 module.exports = authRouter;
