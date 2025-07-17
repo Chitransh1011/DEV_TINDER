2b3933c6eec07170e89e689cea93d28631b5f416
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-07-17 15:21:57 +0530

    Profile route added

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const {  userAuth } = require("../middlewares/auth");
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
 const User = require("../models/user");
+const { validationEditProfile } = require("../utils/validation");
 const profileRouter = express.Router();
 
-profileRouter.get("/profile",userAuth,async(req,res)=>{
-    const user = req.user;
-    res.send(user);
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
+  const user = req.user;
+  res.send(user);
 });
 
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validationEditProfile(req)) {
+      throw new Error("Invalid Edit Input");
+    }
+    const user = req.user;
+    Object.keys(req.body).forEach((key) => (user[key] = req.body[key]));
+    await user.save();
+    res.json({
+      message: `${user.firstName} is updated successfully`,
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,16 +1,20 @@
-const validator = require('validator');
+const validator = require("validator");
 
-const validateSignUp = (req)=>{
-    const {firstName,lastName,emailId,password} = req.body;
+const validateSignUp = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
 
-    if(!firstName || !lastName){
-        throw new Error("Please enter name correctly : ");
-    }
-    else if(!validator.isEmail(emailId)){
-        throw new Error("Invalid Email !!");
-    }
-    else if(!validator.isStrongPassword(password)){
-        throw new Error("Please enter strong password!!");
-    }
-}
-module.exports = {validateSignUp};
+  if (!firstName || !lastName) {
+    throw new Error("Please enter name correctly : ");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Invalid Email !!");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter strong password!!");
+  }
+};
+const validationEditProfile = (req) => {
+  const allowedItem = ["firstName", "lastName", "age", "gender"];
+  const keys = Object.keys(req.body);
+  const isAllowed = keys.every((key) => allowedItem.includes(key));
+  return isAllowed;
+};
+module.exports = { validateSignUp, validationEditProfile };
