dc17f1465ef5230b495fac6c9d9d923e2c7c3bde
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-07-18 16:06:39 +0530

    Added feed option

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const connectionModel = require("../models/connectionrequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -44,12 +45,12 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       .populate("fromUserId")
       .populate("toUserId");
 
-      const data = connectionReq.map((row)=>{
-        if(row.fromUserId._id.equals(loggedIn._id)){
-            return row.toUserId;
-        }
-        return fromUserId;
-      })
+    const data = connectionReq.map((row) => {
+      if (row.fromUserId._id.equals(loggedIn._id)) {
+        return row.toUserId;
+      }
+      return fromUserId;
+    });
     res.json({ data });
   } catch (error) {
     res.json({
@@ -57,5 +58,28 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
   }
 });
-
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const restUser = await User.find();
+    // IMP CONCEPT OF ARROW FUNCTION IS THAT WE CAN RETURN OBJECT BY USING () PARENTHESIS NO NEED OF RETURN
+    const data = restUser
+      .filter((row) => !row._id.equals(req.user._id))
+      .map((row) => ({
+          firstName: row.firstName,
+          lastName: row.lastName,
+          age: row.age,
+          gender: row.gender,
+          photoUrl: row.photoUrl,
+          about: row.about,
+          skills: row.skills,
+      }));
+    res.json({
+      data,
+    });
+  } catch (error) {
+    res.json({
+      message: error.message,
+    });
+  }
+});
 module.exports = userRouter;
